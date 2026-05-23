8dad81ed0bb4c14f60b1f52c1cdfd39e4fb3a65e
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-05-24 00:23:37 +0200

    Main

--- Hw-33/Hw-33.cpp
+++ Hw-33/Hw-33.cpp
@@ -1,20 +1,68 @@
-﻿// Hw-33.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include "Fish.h"
+#include "Bird.h"
+#include "Mammal.h"
+#include "Cage.h"
+#include "ZooException.h"
 
-#include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    try {
+
+        Fish badFish("", false, true);
+
+    }
+    catch (ZooException& ex) {
+
+        cout << ex.showMessage() << endl;
+    }
+
+    cout << "\n====================\n";
+
+
+    try {
+
+        Cage cage1(1, 2);
+
+        Animal* fish =
+            new Fish("Немо", false, true);
+
+        Animal* eagle =
+            new Bird("Орел", true, 120);
+
+        cage1.addAnimal(fish);
+
+        cage1.addAnimal(eagle);
+
+    }
+    catch (ZooException& ex) {
+
+        cout << ex.showMessage() << endl;
+    }
+
+    cout << "\n====================\n";
+
+
+    try {
+
+        Cage cage2(2, 1);
+
+        Animal* tiger =
+            new Mammal("Тигр", true, "Лес");
+
+        Animal* wolf =
+            new Mammal("Волк", true, "Лес");
+
+        cage2.addAnimal(tiger);
+
+        cage2.addAnimal(wolf);
+
+    }
+    catch (ZooException& ex) {
+
+        cout << ex.showMessage() << endl;
+    }
+
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
