8d68bbb0208d2f87f1d5ce753a7b9d3cbccc9cf4
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-05-23 23:52:31 +0200

    Animal.h

--- Hw-33/Animal.h
+++ Hw-33/Animal.h
@@ -1,5 +1,24 @@
 #pragma once
-class Animal
-{
-};
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+class Animal {
+protected:
+    string name;
+    bool predator;
+
+public:
+    Animal(string n, bool p);
+
+    string getName();
+    void setName(string n);
+
+    bool isPredator();
+    void setPredator(bool p);
+
+    virtual void showInfo();
+
+    virtual ~Animal();
+};
