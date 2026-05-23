771a4e9a826b5ce8c31d86fe3978ac8233d2dbca
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-05-24 00:10:27 +0200

    Mammal.cpp

--- Hw-33/Mammal.cpp
+++ Hw-33/Mammal.cpp
@@ -1,1 +1,13 @@
 #include "Mammal.h"
+
+Mammal::Mammal(string n, bool p, string h)
+    : Animal(n, p) {
+
+    habitat = h;
+}
+
+void Mammal::showInfo() {
+
+    cout << "Млекопитающее: " << name << endl;
+    cout << "Среда обитания: " << habitat << endl;
+}
