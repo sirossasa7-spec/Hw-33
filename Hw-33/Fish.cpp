ecfe68671b6242b649f7bb5fbe8880b974018517
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-05-24 00:02:24 +0200

    Fish.cpp

--- Hw-33/Fish.cpp
+++ Hw-33/Fish.cpp
@@ -1,1 +1,17 @@
 #include "Fish.h"
+
+Fish::Fish(string n, bool p, bool d)
+    : Animal(n, p) {
+
+    deepWater = d;
+}
+
+void Fish::showInfo() {
+
+    cout << "Рыба: " << name << endl;
+    cout << "Хищник: "
+        << (predator ? "Да" : "Нет") << endl;
+
+    cout << "Глубоководная: "
+        << (deepWater ? "Да" : "Нет") << endl;
+}
