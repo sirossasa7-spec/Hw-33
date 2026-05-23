6f6f0a0bed70f4b7ab2e0152abf74f2b7de56408
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-05-23 23:56:02 +0200

    Animal.cpp

--- Hw-33/Animal.cpp
+++ Hw-33/Animal.cpp
@@ -1,1 +1,34 @@
 #include "Animal.h"
+#include "ZooException.h"
+Animal::Animal(string n, bool p) {
+
+    if (n.empty()) {
+        throw NameException();
+    }
+
+    name = n;
+    predator = p;
+}
+
+string Animal::getName() {
+    return name;
+}
+
+void Animal::setName(string n) {
+    name = n;
+}
+
+bool Animal::isPredator() {
+    return predator;
+}
+
+void Animal::setPredator(bool p) {
+    predator = p;
+}
+
+void Animal::showInfo() {
+    cout << "Животное: " << name << endl;
+}
+
+Animal::~Animal() {
+}
