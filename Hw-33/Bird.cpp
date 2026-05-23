189c24442a02a496bbcb145afa9de8f562a7bd7e
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-05-24 00:06:39 +0200

    Bird.cpp

--- Hw-33/Bird.cpp
+++ Hw-33/Bird.cpp
@@ -1,1 +1,14 @@
 #include "Bird.h"
+
+Bird::Bird(string n, bool p, int s)
+    : Animal(n, p) {
+
+    flySpeed = s;
+}
+
+void Bird::showInfo() {
+
+    cout << "Птица: " << name << endl;
+    cout << "Скорость полета: "
+        << flySpeed << " км/ч" << endl;
+}
